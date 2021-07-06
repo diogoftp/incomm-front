f0a87914013baf1e93e1dd138f677a325c43b356
Diogo <31069709+diogoftp@users.noreply.github.com>
2021-07-06 15:44:11 -0300

    Initial configuration

---
+++ src/pages/info/index.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { Row, Col } from 'antd';
+
+const Info = (): JSX.Element => {
+  return (
+    <div className='centered'>
+      <Row align='middle' justify='center'>
+        <Col>
+          Gift card info page
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default Info;
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { Row, Col } from 'antd';
+
+const Login = (): JSX.Element => {
+  return (
+    <div className='centered'>
+      <Row align='middle' justify='center'>
+        <Col>
+          Login page
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default Login;
