efc083c501007c868f3fdac5d8da895043ee3900
Diogo <31069709+diogoftp@users.noreply.github.com>
2021-07-11 20:41:48 -0300

    Changed antd language to pt-BR

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import Login from './pages/login';
 import Info from './pages/info';
 import PrivateRoute from './components/privateRoute';
 import './App.css';
+import { ConfigProvider } from 'antd';
+import ptBR from 'antd/lib/locale/pt_BR';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <PrivateRoute exact path="/" component={Info} />
-        <Route path="/login" component={Login} />
-        <PrivateRoute path="/info" component={Info} />
-      </Switch>
-    </Router>
+    <ConfigProvider locale={ptBR}>
+      <Router>
+        <Switch>
+          <PrivateRoute exact path="/" component={Info} />
+          <Route path="/login" component={Login} />
+          <PrivateRoute path="/info" component={Info} />
+        </Switch>
+      </Router>
+    </ConfigProvider>
   );
 }
 
